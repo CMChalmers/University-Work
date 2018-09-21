9158d48c5cfd4cfe0191586373df511b9f3982fa
CMChalmers <34947105+CMChalmers@users.noreply.github.com>
2018-09-21 09:49:53 +0100

    commends added to GameLauncher.java

--- guessingGame/GameLauncher.java
+++ guessingGame/GameLauncher.java
@@ -2,7 +2,9 @@ package guessingGame;
 
 public class GameLauncher {
     public static void main(String[] args){
+        // create new GuessGame object called game
         GuessGame game = new GuessGame();
+        // run the startGame method of GuessGame
         game.startGame();
     }
 }
