d82233a1a6f65aed57ec9652a556b49cb4acbd99
CMChalmers <34947105+CMChalmers@users.noreply.github.com>
2018-09-21 09:51:11 +0100

    Added commends to GuessGame.java

--- guessingGame/GuessGame.java
+++ guessingGame/GuessGame.java
@@ -1,9 +1,11 @@
 package guessingGame;
 
 public class GuessGame {
+    // initialize player object
     Player p1;
 
     public void startGame(){
+        // create new player object
         p1 = new Player();
 
         int guessp1 = 0;
@@ -13,21 +15,26 @@ public class GuessGame {
         int targetNumber = (int) (Math.random() * 10);
         System.out.println("Number to guess is " + targetNumber);
 
-        p1.guess();
+        while (p1.userInput != targetNumber){
+            // loops through game-play loop until user input is equal to the target number.
+            p1.guess();
 
-        guessp1 = p1.userInput;
-        System.out.println("You guessed " + guessp1);
+            guessp1 = p1.userInput;
+            System.out.println("You guessed " + guessp1);
 
-        if(guessp1 == targetNumber){
-            p1isRight = true;
-        }
+            if (guessp1 == targetNumber){
+                // check that guess is equal to random number
+                p1isRight = true;
+            }
 
-        if(p1isRight){
-            System.out.println("We have a winner!");
-            System.out.println("You got it right? " + p1isRight);
-            System.out.println("Game is over.");
-        } else {
-            System.out.println("You will have to try again.");
-        }
-    }
-}
+            if (p1isRight){
+                // what happens if guess is equal to random number
+                System.out.println("We have a winner!");
+                System.out.println("You got it right? " + p1isRight);
+                System.out.println("Game is over.");
+            } else {
+                System.out.println("You will have to try again.");
+            }
+        } // end of while loop
+    } // end of main argument
+} // end of class
