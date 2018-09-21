f584168decea10513b25cae62b8488b47173802f
CMChalmers <34947105+CMChalmers@users.noreply.github.com>
2018-09-21 09:57:07 +0100

    Added comments to Player.java

--- guessingGame/Player.java
+++ guessingGame/Player.java
@@ -1,13 +1,10 @@
-package guessingGame;
-
-import java.util.Scanner;
-
 public class Player {
     int userInput = 0; // where the guess goes
     Scanner reader = new Scanner(System.in);
 
     public void guess(){
         System.out.println("Please enter a number between 1 and 10: ");
+        // take input and cast to type int
         userInput = Integer.parseInt(reader.next());
         System.out.println("I'm guessing " + userInput);
     }
