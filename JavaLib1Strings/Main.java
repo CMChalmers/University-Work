a96cc00d816db72cd4b8cdc3a8d25bc113e20e14
CMChalmers <34947105+CMChalmers@users.noreply.github.com>
2018-10-05 23:45:32 +0100

    Added Comments

--- JavaLib1Strings/Main.java
+++ JavaLib1Strings/Main.java
@@ -3,6 +3,10 @@ package JavaLib1Strings;
 public class Main {
 
     public static boolean compareString(String stringOne, String stringTwo){
+        /* compares two strings, returns true is
+        // they match and false if they dont
+        */
+        
         if(stringOne.equalsIgnoreCase(stringTwo)){
             return true;
         }
@@ -10,6 +14,10 @@ public class Main {
     }
 
     public static void splitArray(String[] list){
+        /* takes an array as a parameter and formats
+        // it to the console seperated by commas,
+        // finished with a full stop
+        */ 
         for(int i = 0; i < list.length; i++){
             if(i == list.length-1){
                 System.out.println(list[i] + ".");
@@ -31,7 +39,6 @@ public class Main {
         }
 
         // test to compare arraySplit
-
         String[] list = new String[4];
         list[0] = "dog";
         list[1] = "cat";
